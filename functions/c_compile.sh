3791ab9988bfaa2c203135e55e2cb3b2dc798bd8
YOSHIHIDEShimoji <g.y.shimoji@gmail.com>
2025-06-11 00:13:37 +0900

    add some change func/c.sh

--- functions/c_compile.sh
+++ functions/c_compile.sh
@@ -1,7 +1,16 @@
 c() {
-  [[ "$1" == *.c ]] || { echo "Usage: c file.c"; return 1; }
-  fullpath="$(realpath "$1")"
-  out="${fullpath%.c}"
-  cc "$fullpath" -o "$out" && "$out"
-}
+  [[ "$1" == *.c ]] || { echo "Usage: c file.c [args ...]"; return 1; }
 
+  src="$(realpath "$1")"
+  shift  # 残りの引数を実行ファイルに渡すためにずらす
+
+  dir="$(dirname "$src")"
+  name="$(basename "${src%.c}")"
+
+  build_dir="$dir/build"
+  mkdir -p "$build_dir"
+
+  out="$build_dir/$name"
+
+  cc "$src" -o "$out" && "$out" "$@"
+}
